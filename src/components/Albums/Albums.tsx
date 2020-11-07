a13c64a274f169162f1bcd54d3e4bad71e85322c
Woody Butler <forrestwbutler@gmail.com>
2020-11-07 10:06:56 -0800

    Update albums container to use API method

--- src/components/Albums/Albums.tsx
+++ src/components/Albums/Albums.tsx
@@ -2,6 +2,7 @@ import RenderDisplayAlbum from "../DisplayAlbum/RenderDisplayAlbum";
 import React, { useState, useEffect } from "react";
 import "./style.scss";
 import { Album } from 'src/types/Album';
+import BackendApiClient from 'src/api/api';
 
 const axios = require("axios").default;
 
@@ -10,8 +11,8 @@ export const RenderAlbums: React.FunctionComponent<{}> = () => {
   useEffect(() => {
     // TODO change to backendapiclient call
     const fetchData = async () => {
-      const result = await axios("http://127.0.0.1:8000/api/albums/");
-      setAlbums(result.data);
+      const result = await BackendApiClient.getAlbums();
+      setAlbums(result);
     };
     fetchData();
   }, []);
